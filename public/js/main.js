5c07a1e6e1973e6853847b13289c356aad43073a
nazmi285 <m.nazmi285@gmail.com>
2021-08-12 23:24:29 +0800

    push api study

--- public/js/main.js
+++ public/js/main.js
@@ -13,6 +13,16 @@ window.onload = () => {
 			// console.log('Service Worker is registered', swReg);
 			// displayNotification();
 			initializeUi();
+
+			reg.pushManager.getSubscription().then(function(sub) {
+				if (sub === null) {
+					// Update UI to ask user to register for Push
+					console.log('Not subscribed to push service!');
+				} else {
+					// We have a subscription, update the database
+					console.log('Subscription object: ', sub);
+				}
+			});
 		})
 		.catch(error => {
 			console.error('Service Worker Error', error);
@@ -66,4 +76,25 @@ window.onload = () => {
 			Notification.requestPermission();
 		}
 	}
+
+	function subscribeUser() {
+		if ('serviceWorker' in navigator) {
+			navigator.serviceWorker.ready.then(function(reg) {
+
+				reg.pushManager.subscribe({
+					userVisibleOnly: true
+				}).then(function(sub) {
+					console.log('Endpoint URL: ', sub.endpoint);
+				}).catch(function(e) {
+					if (Notification.permission === 'denied') {
+						console.warn('Permission for notifications was denied');
+					} else {
+						console.error('Unable to subscribe to push', e);
+					}
+				});
+			})
+		}
+	}
+
+
 }
