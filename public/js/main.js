900f4b9299fcac6d0fccd90840166f5d400772b3
nazmi285 <m.nazmi285@gmail.com>
2021-08-14 09:52:19 +0800

    register gelocation 6

--- public/js/main.js
+++ public/js/main.js
@@ -112,7 +112,7 @@ window.onload = () => {
     	var latlon = pos.coords.latitude + "," + pos.coords.longitude;
 
     	var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
-    	+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_:KEY";
+    	+latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyCtvBQmouvE53r5ObVb3Cv4PFVFR_Y5YiU";
 
 	    var map = document.querySelector("mapholder");
 	    map.innerHTML = "<img src='"+img_url+"'>";
