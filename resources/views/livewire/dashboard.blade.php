3c461ccdc47962d98df9d5eaf35a87d17f2f7c02
nazmi285 <m.nazmi285@gmail.com>
2021-08-14 10:30:36 +0800

    register gelocation 10

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -42,7 +42,7 @@
     }
 
     function codeLatLng(lat, lng) {
-
+        alert(lat, lng);
         var latlng = new google.maps.LatLng(lat, lng);
         geocoder.geocode({'latLng': latlng}, function(results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
