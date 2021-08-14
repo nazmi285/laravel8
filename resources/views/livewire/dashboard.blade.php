36b80e9aba7fe2c6a60aeb88fd1b44448b2dde89
nazmi285 <m.nazmi285@gmail.com>
2021-08-14 09:23:03 +0800

    register gelocation 2

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -19,3 +19,23 @@
     @endforeach
 </div>
 
+<script type="text/javascript">
+    //request for location
+    getLocation();
+    //function that gets the location and returns it
+        function getLocation() {
+            if(navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                console.log("Geo Location not supported by browser");
+            }
+        }
+        //function that retrieves the position
+        function showPosition(position) {
+            var location = {
+                longitude: position.coords.longitude,
+                latitude: position.coords.latitude
+            }
+            console.log(location)
+        }
+</script>
