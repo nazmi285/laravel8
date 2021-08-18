c034476aab852909579c37778de0b59a5acf0577
nazmi285 <m.nazmi285@gmail.com>
2021-08-18 22:06:00 +0800

    revert changes at button notification

--- resources/views/livewire/notification.blade.php
+++ resources/views/livewire/notification.blade.php
@@ -38,6 +38,6 @@
 	</div>
 
 	<div class="col-md-8 d-grid p-3">
-		<button type="button" class="btn btn-primary rounded-0" id="enableNotifications-xx" wire:click="notify">🔔 Enable Notification</button>
+		<button type="button" class="btn btn-primary rounded-0" id="enableNotifications" >🔔 Enable Notification</button>
 	</div>
 </div>
