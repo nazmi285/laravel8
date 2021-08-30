80b440d286b90b4d7294744398b23e9329341a3b
nazmi285 <m.nazmi285@gmail.com>
2021-08-30 17:08:46 +0800

    correction on config name google redirection

--- config/services.php
+++ config/services.php
@@ -42,7 +42,7 @@ return [
     'google' => [
         'client_id' =>  env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' =>   env('GOOGLE_CLIENT_REDIERECT'),
+        'redirect' =>   env('GOOGLE_CLIENT_REDIRECT'),
     ],
 
 ];
